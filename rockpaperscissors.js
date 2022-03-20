84eb376673085c3cf12d28fbeac4faa4640cd99a
Michael Pabs <pabsz07mike@gmail.com>
2022-03-20 20:53:17 +0800

    Modified for cleaner code and done some error capture

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,62 +3,33 @@ let userScore=0;
 let compScore=0;
 
 function computerPlay(){
-    let compSelection= Math.floor(Math.random()*3)+1;
-    if(compSelection===1)
+    let compSelection= Math.floor(Math.random()*3);
+    
+    if(compSelection===0)
     return "rock";
-    else if (compSelection ===2)
+    else if (compSelection===1)
     return 'paper';
-    else if (compSelection===3)
+    else if (compSelection===2)
     return "scissors";
 }
 
 function playRound(playerSelection,computerSelection){
      if(playerSelection!==computerSelection)
      {
-        if(playerSelection==='rock')
+        if((playerSelection==='rock'&&computerSelection==='paper') || (playerSelection==='paper'&&computerSelection==='scissors') || (playerSelection==='scissors'&&computerSelection==='rock'))
         {
-            if(computerSelection==='paper')
-            {
-                alert("You lose! Paper beats rock");
-                compScore++;
-            }
-            else if(computerSelection==='scissors')
-            {
-                alert("You win! Rock beats scissors");
-                userScore++;
-            }
+            compScore+=1;
+            console.log(`You lose! ${computerSelection.replace(computerSelection[0],computerSelection[0].toUpperCase())} beats ${playerSelection}`);
         }
-        else if(playerSelection==='paper')
+        else if((playerSelection==='scissors'&&computerSelection==='paper') || (playerSelection==='paper'&&computerSelection==='rock') || (playerSelection==='rock'&&computerSelection==='scissors'))
         {
-            if(computerSelection==='rock')
-            {
-                alert("You win! Paper beats rock");
-                userScore++;
-            }
-            else if(computerSelection==='scissors')
-            {
-                alert("You lose! Scissors beats paper");
-                compScore++;
-            }
+            userScore+=1;
+            console.log(`You win! ${playerSelection.replace(playerSelection[0],playerSelection[0].toUpperCase())} beats ${computerSelection}`);
         }
-        else if(playerSelection==='scissors')
-        {
-            if(computerSelection==='paper')
-            {   
-                alert("You win! scissors beats paper");
-                userScore++;
-            }
-            else if(computerSelection==='rock')
-            {
-                alert("You lose! Rock beats scissors");
-                compScore++;
-            }
-        }
+        
      }
      else
-     {
-         alert(`No one wins. You both have ${computerSelection}`);
-     }
+        console.log(`No one wins. You both have ${computerSelection}`);
 }
 
 function game(){
@@ -67,31 +38,43 @@ function game(){
     let check=0;
     compScore=0;
     userScore=0;
-
-    for(let i=0;i<5;i++)
+    
+    if(confirm("Start the game?"))
     {
-        computerSelection=computerPlay();
-        playerSelection=prompt("Enter your bet:", "Rock,paper or scissors");
-        if(playerSelection!==null)
-        playerSelection=playerSelection.toLowerCase();
-        else 
+        for(let i=0;i<5;i++)
         {
-            alert("You exited the game!");
-            break;
-        }
-
-        check=arrSelection.indexOf(playerSelection);
-        while(check===-1)
-        {
-            alert("Wrong input please enter either rock, paper or scissors!");
+            computerSelection=computerPlay();
             playerSelection = prompt("Enter your bet:", "Rock,paper or scissors");
             if(playerSelection!==null)
-            playerSelection=playerSelection.toLowerCase();
+            {
+                playerSelection=playerSelection.toLowerCase();
+                check = arrSelection.indexOf(playerSelection);
+            }
             else break;
-            check=arrSelection.indexOf(playerSelection);
+            while(check===-1)
+            {
+                alert("Wrong input please enter either rock, paper or scissors!");
+                playerSelection = prompt("Enter your bet:", "Rock,paper or scissors");
+                if(playerSelection!==null)
+                {
+                    playerSelection=playerSelection.toLowerCase();
+                    check=arrSelection.indexOf(playerSelection);
+                }
+                else
+                {
+                    check=3;
+                    break;
+                }
+            }
+
+            if(check===3) break;
+
+            playRound(playerSelection,computerSelection);
+            console.log(`Round ${(i+1)} - Your score: ${userScore} | Opponent Score: ${compScore}`);
+
         }
-        playRound(playerSelection,computerSelection);
-        console.log(`Round ${(i+1)} - Your score: ${userScore} | Opponent Score: ${compScore}`);
+        compScore<userScore?alert('Congratulations! You win'):compScore===userScore?alert('No one wins'):alert('Uh oh! You lose');
     }
-    compScore<userScore?alert('Congratulations! You win'):alert ('Uh oh! You lose!');
+    else 
+        alert("You exited the game!");
 }
